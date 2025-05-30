1c46d61a8bab2a7e8e405771b720b53cadaab05b
sandom081 <yugtandel686@gmail.com>
2025-05-30 18:52:56 +0530

    ww

--- js/script.js
+++ js/script.js
@@ -90,3 +90,18 @@ document.querySelectorAll('.mobile-nav a').forEach(link => {
     }, 300);  // adjust time if nav close animation time is different
   });
 });
+
+  let lastScrollY = window.scrollY;
+  const header = document.querySelector('.main-header');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > lastScrollY) {
+      // Scrolling down
+      header.style.top = "-100px";
+    } else {
+      // Scrolling up
+      header.style.top = "0";
+    }
+    lastScrollY = window.scrollY;
+  });
+
